2fbf3d45cdb76ee7be90a603e70be7f4a858eaab
leawron <leawron@163.com>
2025-09-25 19:14:15 +0800

    feat(psycho.main): session entry
    通过 main 进入 session

---
+++ psycho/main.py
@@ -0,0 +1,4 @@
+from psycho.session import main as session_entry
+
+if __name__ == "__main__":
+    session_entry()
--- psycho/session.py
+++ psycho/session.py
@@ -1,17 +1,20 @@
 import multiprocessing
 from pathlib import Path
 
-from lsl_recv import main as lsl_recv_main
 from psychopy import session
+
+from psycho.lsl_recv import main as lsl_recv_main
 
 
 def start_session():
     """启动实验会话"""
     sess = session.Session(Path("./").absolute())
-    a = sess.addExperiment("gng.py", key="go-nogo")
-    print(a)
+    exp_root = Path(__file__).parent / "exps"
+    sess.addExperiment(exp_root / "gng.py", key="go-nogo")
+    sess.addExperiment(exp_root / "nback.py", key="nback")
 
     # start_listener()
+    # 会阻塞，直到所有实验结束
     sess.start()
 
 
